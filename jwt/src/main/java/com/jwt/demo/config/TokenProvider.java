26fd8ed19c842a9815e8271cb337a35ddd18909a
Nidura Prageeth <niduraprageeth@gmail.com>
2018-10-18 17:10:35 +0530

    Auth controller and token provider create

--- jwt/src/main/java/com/jwt/demo/config/TokenProvider.java
+++ jwt/src/main/java/com/jwt/demo/config/TokenProvider.java
@@ -1,7 +1,76 @@
 package com.jwt.demo.config;
 
+import com.jwt.demo.model.ParseToken;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.function.Function;
+
+import static com.jwt.demo.constants.Constants.ACCESS_TOKEN_VALIDITY_SECONDS;
+import static com.jwt.demo.constants.Constants.SIGNING_KEY;
+
 /**
  * Created by Nidura on 2018-10-18.
  */
-public class TokenProvider {
+@Component
+public class TokenProvider implements Serializable {
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SIGNING_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(ParseToken parseToken) {
+        Claims claims = Jwts.claims()
+                .setSubject(parseToken.getName());
+        claims.put("nic", String.valueOf(parseToken.getNic()));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY_SECONDS * 1000))
+                .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        final String username = getUsernameFromToken(token);
+        return (
+                username != null
+                        && !isTokenExpired(token));
+    }
+
+    public UsernamePasswordAuthenticationToken getAuthentication(final String token) {
+        String user = Jwts.parser()
+                .setSigningKey(SIGNING_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+        return new UsernamePasswordAuthenticationToken(user, "", null);
+    }
 }
--- jwt/src/main/java/com/jwt/demo/controller/AuthenticationController.java
+++ jwt/src/main/java/com/jwt/demo/controller/AuthenticationController.java
@@ -1,6 +1,15 @@
 package com.jwt.demo.controller;
 
+import com.jwt.demo.config.TokenProvider;
+import com.jwt.demo.model.AuthToken;
+import com.jwt.demo.model.ParseToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/token")
 public class AuthenticationController {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ResponseEntity<?> register(@RequestBody ParseToken parseToken) throws AuthenticationException {
+        String token = tokenProvider.generateToken(parseToken);
+        SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(token));
+
+        return ResponseEntity.ok(new AuthToken(token));
+    }
 
 }
